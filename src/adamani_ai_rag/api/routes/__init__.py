4c2323f44e39abea1c5fa11412be24d9749c63b8
gahsilas <gahsilas@gmail.com>
2025-12-07 16:08:18 +0000

    Issues with imports

--- src/adamani_ai_rag/api/routes/__init__.py
+++ src/adamani_ai_rag/api/routes/__init__.py
@@ -1,1 +1,5 @@
-
+from .health import router as health
+from .chat import router as chat
+from .documents import router as documents
+from .auth import router as auth
+from .invoices import router as invoices
